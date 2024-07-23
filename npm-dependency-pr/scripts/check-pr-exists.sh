2d3b296aad5153118f56af1cd54724d45ddf44da
Tim Rowe <tim@tjsr.id.au>
2024-07-23 10:17:18 +1000

    external script with input checks.

--- npm-dependency-pr/scripts/check-pr-exists.sh
+++ npm-dependency-pr/scripts/check-pr-exists.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
-BRANCH_NAME="dependency/${{ inputs.dependency }}/$VERSION"
-BRANCH_HEAD="${{ steps.get-branch.outputs.branch }}"
+if [ -z "$GITHUB_OUTPUT" ]; then
+  if [ "$1" != "stdout" ]; then
+    echo "GITHUB_OUTPUT is not set. To send to stdout, use 'stdout' as param'"
+    exit 2
+  fi
+  GITHUB_OUTPUT="/dev/stdout"
+fi
 
-prs=$(gh pr list \
+if [ -z "$GITHUB_REPOSITORY" ]; then
+    echo "GITHUB_REPOSITORY must be set"
+    exit 1
+fi
+
+if [ -z "$BRANCH_HEAD" ]; then
+    echo "BRANCH_HEAD must be set.  It must poit to the name of the branch that the PR will be created from."
+    exit 1
+fi
+
+if [ -z "$BRANCH_NAME" ]; then
+  echo "BRANCH_NAME must be set.  It must point to the name of the branch that the PR will be created to."
+  exit 1
+fi
+
+echo "Checking for PR in $GITHUB_REPOSITORY at $BRANCH_NAME on $BRANCH_HEAD"
+# BRANCH_NAME="dependency/${{ inputs.dependency }}/$VERSION"
+# BRANCH_HEAD="${{ steps.get-branch.outputs.branch }}"
+
+prCount=$(gh pr list \
     --repo "$GITHUB_REPOSITORY" \
-    --head '${{ steps.get-branch.outputs.branch }}' \
+    --head "$BRANCH_HEAD" \
+    --json number --jq '. | length'
     )
-if [ "$prs" -gt 0 ]; then
+
+if [ "$prCount" -gt 0 ]; then
     echo "Branch already exists at $BRANCH_NAME - no PR will be created."
     echo "exists=true" >> "$GITHUB_OUTPUT"
 else
