2c3d38ea56b5d518e3e06d9828d2b49471b79859
Tim Rowe <tim@tjsr.id.au>
2024-08-01 18:42:26 +1000

    Improvements to retrieving the latest public version to prevent clashes. (#86)
    * Get latest version from pacakges before bumping.
    
    * Fix quote; Fix action location.
    
    * Get package name and pass to publishd version retrieve.
    
    * Package param was specified twice.
    
    * Move package.json read up to use in step name.
    
    * Pass github auth token to  get package version.
    
    * Switch to own vrsion implementation.
    
    * Move script to external files.
    
    * Set perms to +x
    
    * Fix using present multiple times.
    
    * Expect NEXT_VERSION_NUMBER not LATEST_VERSION.
    
    * Missing space in if statement.
    
    * Better handling of unique versions that won't conflict with published version.
    
    * Cleared up log message.
    
    * Correct use of quotes in vars.
    
    * Debug output.
    
    * Fix quotes in output value.
    
    * Pass preid to current-package script.
    
    * Add preid for missing input.
    
    * Pass patchlevel to current-package.sh and check for it being provided.

---
+++ version-bump-publish/scripts/bump-version.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ -z "$GITHUB_OUTPUT" ]; then
+  GITHUB_OUTPUT="/dev/stdout"
+fi
+
+if [ -z "$PATCHLEVEL" ]; then
+  echo "PATCHLEVEL is not set. Please set the PATCHLEVEL environment variable of patchlevel action input."
+  exit 1
+fi
+
+if [ -z "$PACKAGE_NAME" ]; then
+  echo "PACKAGE_NAME is not set. Please set the PACKAGE_NAME environment variable of current-package step output."
+  exit 1
+fi
+
+if [ "$NO_TAG" = "true" ] || [ "$NO_TAG" = "--no-git-tag-version" ]; then
+  NO_TAG="--no-git-tag-version"
+else
+  NO_TAG=""
+fi
+
+if [ -z "$NEXT_VERSION_NUMBER" ]; then
+  if [ -z "$BRANCH_VERSION" ]; then
+    echo "BRANCH_VERSION is not set. Please set the BRANCH_VERSION or NEXT_VERSION_NUMBER."
+    exit 1
+  fi
+  if [ "$PREID" = "release" ]; then
+    PREID_SWITCH=""
+  elif [ ! -z "$PREID" ]; then
+    echo "Using prerelease identifier: $PREID"
+    PREID_SWITCH="--preid $PREID"
+  else
+    PREID_SWITCH="--preid dev"
+  fi
+
+  NEXT_VERSION_NUMBER=$(npx semver --increment $PATCHLEVEL $PREID_SWITCH $BRANCH_VERSION)
+fi
+
+npm version $NEXT_VERSION_NUMBER $NO_TAG
+echo "version=$NEXT_VERSION_NUMBER" >> "$GITHUB_OUTPUT"
+echo "fullVersion=$PACKAGE_NAME@$NEXT_VERSION_NUMBER" >> "$GITHUB_OUTPUT"
+echo "Project version number bumped to $PACKAGE_NAME@$NEXT_VERSION_NUMBER"
